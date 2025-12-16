b61eb96952c1884ca533712c9848e20a2936f698
Sergey <tac1402@gmail.com>
2025-12-16 21:23:14 +0200

    +docs

--- TacBattleAI/Assets/_TacLibrary/TacItemMove/Code/Turn/ItemTurn.cs
+++ TacBattleAI/Assets/_TacLibrary/TacItemMove/Code/Turn/ItemTurn.cs
@@ -1,3 +1,6 @@
+// Author: Sergej Jakovlev <tac1402@gmail.com>
+// Copyright (C) 2025-26 Sergej Jakovlev
+
 using System.Collections;
 using System.Collections.Generic;
 using Tac.Camera;
@@ -11,14 +14,7 @@ namespace Tac.ItemMove
 	{
 		public KeyCode RotateKey = KeyCode.Mouse1;
 
-		public TopCamera CameraManager;
-
 		private Item2 selectedItem;
-
-		void Awake()
-		{
-			//CameraManager.OnItemTap += OnItemTap;
-		}
 
 
 		void Update()
