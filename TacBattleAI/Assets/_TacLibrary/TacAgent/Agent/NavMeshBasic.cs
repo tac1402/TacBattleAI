adb2880fe2eded28f1d66cd5bbcbb14c9d4453ef
Sergey <tac1402@gmail.com>
2025-12-23 00:58:33 +0200

    +NavMesh

---
+++ TacBattleAI/Assets/_TacLibrary/TacAgent/Agent/NavMeshBasic.cs
@@ -0,0 +1,36 @@
+﻿// Author: Sergej Jakovlev <tac1402@gmail.com>
+// Copyright (C) 2018-24 Sergej Jakovlev
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.AI.Navigation;
+
+namespace Tac.Agent
+{
+	public class NavMeshBasic : MonoBehaviour
+	{
+		public NavMeshSurface surface;
+
+		private void Start()
+		{
+			CreateSurface();
+			UpdateSurface();
+		}
+
+		public void CreateSurface()
+		{
+			if (surface != null)
+			{
+				surface.BuildNavMesh();
+			}
+		}
+		public void UpdateSurface()
+		{
+			if (surface != null)
+			{
+				surface.UpdateNavMesh(surface.navMeshData);
+			}
+		}
+	}
+}
